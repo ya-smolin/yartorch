cc15ec0df36dbeabdb66e5672b59cfdaeb41dcde
sysgen <yaroslavsmolin@smoothr.de>
2023-11-04 17:17:09 +0100

    train_lora_zephyr in progress

--- lab2.py
+++ lab2.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig, TrainingArguments, Trainer
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig, TrainingArguments, Trainer, pipeline
 import torch
 import time
 import evaluate
@@ -7,13 +7,13 @@ import pandas as pd
 import numpy as np
 
 huggingface_dataset_name = "knkarthick/dialogsum"
-
 dataset = load_dataset(huggingface_dataset_name)
+model_path = "/home/sysgen/Pycharm/wotspace/text-generation-webui/models/HuggingFaceH4_zephyr-7b-beta"
+pipe = pipeline("text-generation", model=model_path, torch_dtype=torch.bfloat16, device_map="auto")
 
-model_name='google/flan-t5-base'
+original_model = pipe.model
+tokenizer = pipe.tokenizer
 
-original_model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # In[]:
 def print_number_of_trainable_model_parameters(model):
@@ -25,6 +25,7 @@ def print_number_of_trainable_model_parameters(model):
             trainable_model_params += param.numel()
     return f"trainable model parameters: {trainable_model_params}\nall model parameters: {all_model_params}\npercentage of trainable model parameters: {100 * trainable_model_params / all_model_params:.2f}%"
 
+
 print(print_number_of_trainable_model_parameters(original_model))
 
 # In[]:
@@ -33,13 +34,7 @@ index = 200
 dialogue = dataset['test'][index]['dialogue']
 summary = dataset['test'][index]['summary']
 
-prompt = f"""
-Summarize the following conversation.
-
-{dialogue}
-
-Summary:
-"""
+prompt = f"You summarize the following conversation.\n\n{dialogue}\n\nSummary: "
 
 inputs = tokenizer(prompt, return_tensors='pt')
 output = tokenizer.decode(
@@ -74,58 +69,50 @@ def tokenize_function(example):
 # The dataset actually contains 3 diff splits: train, validation, test.
 # The tokenize_function code is handling all data across all splits in batches.
 
-# dataset_short = dataset #.filter(lambda example, index: index % 10 == 0, with_indices=True)
-# tokenized_datasets = dataset_short.map(tokenize_function, batched=True)
-# tokenized_datasets = tokenized_datasets.remove_columns(['id', 'topic', 'dialogue', 'summary', ])
-# output_dir = f'./dialogue-summary-training-{str(int(time.time()))}'
-
+dataset_short = dataset  # .filter(lambda example, index: index % 10 == 0, with_indices=True)
+tokenized_datasets = dataset_short.map(tokenize_function, batched=True)
+tokenized_datasets = tokenized_datasets.remove_columns(['id', 'topic', 'dialogue', 'summary', ])
+output_dir = f'./dialogue-summary-training-{str(int(time.time()))}'
 
 # In[]:
 from peft import LoraConfig, get_peft_model, TaskType, PeftModel
-#
-# lora_config = LoraConfig(
-#     r=32, # Rank
-#     lora_alpha=32,
-#     target_modules=["q", "v"],
-#     lora_dropout=0.05,
-#     bias="none",
-#     task_type=TaskType.SEQ_2_SEQ_LM # FLAN-T5
-# )
-#
-# peft_model = get_peft_model(original_model, lora_config)
-# print(print_number_of_trainable_model_parameters(peft_model))
-#
-# output_dir = f'./peft-dialogue-summary-training-{str(int(time.time()))}'
-#
-# peft_training_args = TrainingArguments(
-#     output_dir=output_dir,
-#     auto_find_batch_size=True,
-#     learning_rate=1e-3,  # Higher learning rate than full fine-tuning.
-#     num_train_epochs=1,
-#     #logging_steps=1,
-#     #max_steps=1
-# )
-#
-# peft_trainer = Trainer(
-#     model=peft_model,
-#     args=peft_training_args,
-#     train_dataset=tokenized_datasets["train"],
-#     eval_dataset=tokenized_datasets["validation"]
-# )
-#
-# peft_trainer.train()
-#
-# peft_model_path="./peft-dialogue-summary-checkpoint-local"
-#
-# peft_trainer.model.save_pretrained(peft_model_path)
-# tokenizer.save_pretrained(peft_model_path)
 
-peft_model_base = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base", torch_dtype=torch.bfloat16)
-tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
-instruct_model = PeftModel.from_pretrained(peft_model_base,
-                                       '/home/yaro/workspace/yartorch/Generative-AI-with-LLMs/Week-2/peft-dialogue-summary-training-1688685236/checkpoint-1500',
-                                       torch_dtype=torch.bfloat16,
-                                       is_trainable=False)
+lora_config = LoraConfig(
+    r=32,  # Rank
+    lora_alpha=32,
+    target_modules=["q", "v"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type=TaskType.SEQ_2_SEQ_LM  # FLAN-T5
+)
+
+peft_model = get_peft_model(original_model, lora_config)
+print(print_number_of_trainable_model_parameters(peft_model))
+
+output_dir = f'./peft-dialogue-summary-training-{str(int(time.time()))}'
+
+peft_training_args = TrainingArguments(
+    output_dir=output_dir,
+    auto_find_batch_size=True,
+    learning_rate=1e-3,  # Higher learning rate than full fine-tuning.
+    num_train_epochs=1,
+    # logging_steps=1,
+    # max_steps=1
+)
+
+peft_trainer = Trainer(
+    model=peft_model,
+    args=peft_training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"]
+)
+
+peft_trainer.train()
+
+peft_model_path = "./peft-dialogue-summary-checkpoint-local"
+
+peft_trainer.model.save_pretrained(peft_model_path)
+tokenizer.save_pretrained(peft_model_path)
 
 # In[]:
 index = 200
@@ -141,16 +128,12 @@ Summary: """
 
 input_ids = tokenizer(prompt, return_tensors="pt").input_ids
 
-original_model_outputs = original_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200, num_beams=1))
+original_model_outputs = original_model.generate(input_ids=input_ids,
+                                                 generation_config=GenerationConfig(max_new_tokens=200, num_beams=1))
 original_model_text_output = tokenizer.decode(original_model_outputs[0], skip_special_tokens=True)
-
-instruct_model_outputs = instruct_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200, num_beams=1))
-instruct_model_text_output = tokenizer.decode(instruct_model_outputs[0], skip_special_tokens=True)
 
 print(dash_line)
 print(f'ORIGINAL MODEL:\n{original_model_text_output}')
-print(dash_line)
-print(f'INSTRUCT MODEL:\n{instruct_model_text_output}')
 
 dialogues = dataset['test'][0:10]['dialogue']
 human_baseline_summaries = dataset['test'][0:10]['summary']
@@ -173,13 +156,7 @@ original_model_outputs = original_model.generate(input_ids=input_ids,
                                                  generation_config=GenerationConfig(max_new_tokens=200))
 original_model_text_output = tokenizer.decode(original_model_outputs[0], skip_special_tokens=True)
 
-instruct_model_outputs = instruct_model.generate(input_ids=input_ids,
-                                                 generation_config=GenerationConfig(max_new_tokens=200))
-instruct_model_text_output = tokenizer.decode(instruct_model_outputs[0], skip_special_tokens=True)
-
-
 original_model_summaries.append(original_model_text_output)
-instruct_model_summaries.append(instruct_model_text_output)
 
 # In[]:
 rouge = evaluate.load('rouge')
@@ -191,15 +168,5 @@ original_model_results = rouge.compute(
     use_stemmer=True,
 )
 
-instruct_model_results = rouge.compute(
-    predictions=instruct_model_summaries,
-    references=human_baseline_summaries[0:len(instruct_model_summaries)],
-    use_aggregator=True,
-    use_stemmer=True,
-)
-
-
 print('ORIGINAL MODEL:')
 print(original_model_results)
-print('INSTRUCT MODEL:')
-print(instruct_model_results)
